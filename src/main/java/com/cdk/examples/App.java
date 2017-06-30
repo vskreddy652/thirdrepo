a21aa98b979af3a271cdf754a26817859fc000d4
vskreddy652 <vskreddy652@yahoo.com>
2017-07-01 02:49:26 +0530

    simple commit message1
    Signed-off-by: suresh <vskreddy652@yahoo.com>

---
+++ src/main/java/com/cdk/examples/App.java
@@ -0,0 +1,14 @@
+package com.cdk.examples;
+
+/**
+ * Hello world!sssssssssssssssss
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Something got changed, Can you observe" );
+        new Avasarama().show();
+    }
+}
---
+++ src/main/java/com/cdk/examples/Avasarama.java
@@ -0,0 +1,10 @@
+package com.cdk.examples;
+
+/**
+ * Created by ADMIN on 6/29/17.
+ */
+public class Avasarama {
+    void show(){
+        System.out.println("Edo vedi badha...");
+    }
+}
