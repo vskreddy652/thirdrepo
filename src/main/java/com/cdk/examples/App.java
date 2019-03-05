e709acfb82f2019572e07693782b8bf2c1f7d929
vskreddy652 <vskreddy652@yahoo.com>
2019-03-05 11:59:14 +0530

    Update App.java

--- src/main/java/com/cdk/examples/App.java
+++ src/main/java/com/cdk/examples/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Something got changed, Can you observe sssss");
+        System.out.println( "Hello World! Something got changed, Can you observe jhydygtasjdygatusdyg sssss");
         new Avasarama().show();
     }
 }
